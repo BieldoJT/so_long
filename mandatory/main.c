e8ec496a70b3f1d8a1a23ea9c43185e6d685f489
Gabriel <gabrieldaconceicaodeoliveira@gmail.com>
2025-01-11 08:51:35 -0300

    função usando o keycode pra verificar qual o input no pc de lá//usar essa função pra saber os botoes no linux

--- mandatory/main.c
+++ mandatory/main.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gda-conc <gda-conc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bieldojt <bieldojt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 23:18:47 by gda-conc          #+#    #+#             */
-/*   Updated: 2025/01/08 13:05:14 by gda-conc         ###   ########.fr       */
+/*   Updated: 2025/01/11 08:50:38 by bieldojt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+//usar essa função pra saber os botoes no linux
+int print_button(int KeyCode)
+{
+	printf("%d\n", KeyCode);
+
+	return 0;
+}
 
 int	main(int argc, char **argv)
 {
@@ -25,6 +33,8 @@ int	main(int argc, char **argv)
 	init_sprites(game);
 	render(game);
 	mlx_key_hook(game->win_ptr, input_game, game);
+	//usar essa função pra saber os botoes no linux
+	//mlx_key_hook(game->win_ptr, print_button, game);
 	mlx_hook(game->win_ptr, 17, 0, destroy_mlx_and_game, game);
 	mlx_loop(game->mlx_ptr);
 	return (0);
